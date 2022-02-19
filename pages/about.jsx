2f842ac1784d7d40cea857c224d205ea13d6d901
dosyao <logan0mcv@gmail.com>
2022-02-19 19:25:27 +0200

    added title

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,13 @@
 import { serialize } from "next-mdx-remote/serialize";
+import Head from "next/head";
 import InformationComponent from '../components/InformationComponent';
 
-const About = ({ source }) => <InformationComponent source={source} />;
+const About = ({ source }) => <>
+    <Head>
+        <title>About Us | Newspaper</title>
+    </Head>
+    <InformationComponent source={source} />
+</>;
 
 export const getStaticProps = async ({ pathname = '' }) => {
     const source = (await import(`/public/data/info-pages/${pathname || 'about'}.js`))?.default;
