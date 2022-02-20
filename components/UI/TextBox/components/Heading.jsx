49cd122247285dd98edf7f2e20997e16c7e09abf
dosyao <logan0mcv@gmail.com>
2022-02-20 19:49:57 +0200

    fix font-weight

--- components/UI/TextBox/components/Heading.jsx
+++ components/UI/TextBox/components/Heading.jsx
@@ -3,11 +3,11 @@ const Heading = ({ jsxTag, ...props }) => {
 
     switch (jsxTag) {
         case 'h1':
-            return <h1 {...props} id={id} className="mt-0 mb-5 text-xl font-semibold lg:text-2xl lg:font-black lg:mb-7" />;
+            return <h1 {...props} id={id} className="mt-0 mb-5 text-xl font-black lg:text-2xl lg:mb-7" />;
         case 'h2':
-            return <h2 {...props} id={id} className="mt-0 mb-2 text-lg font-semibold lg:text-xl lg:font-black lg:mb-5" />;
+            return <h2 {...props} id={id} className="mt-0 mb-2 text-lg font-black lg:text-xl lg:mb-5" />;
         case 'h3':
-            return <h3 {...props} id={id} className="mt-0 mb-2 text-base font-semibold lg:text-lg lg:font-black lg:mb-5" />;
+            return <h3 {...props} id={id} className="mt-0 mb-2 text-base font-black lg:text-lg lg:mb-5" />;
         default:
             return null;
     }
