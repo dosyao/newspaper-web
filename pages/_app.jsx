9895ba5f114c5dae1878d15ee61da1fbba6fcc14
dosyao <logan0mcv@gmail.com>
2022-02-20 21:53:53 +0200

    removed unscalable no

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -23,7 +23,7 @@ const App = ({ Component, pageProps }) => {
     return (
         <DisplaySize>
             <Head>
-                <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover" />
+                <meta name="viewport" content="width=device-width" />
             </Head>
             <Component {...pageProps} />
             {!cookieRead && <CookieModal onClick={handleCookie} />}
