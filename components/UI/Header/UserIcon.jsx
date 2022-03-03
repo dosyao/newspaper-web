4dc506044fb959f81cb8509db24b97238502a3cd
dosyao <logan0mcv@gmail.com>
2022-03-03 23:27:45 +0200

    added logout

--- components/UI/Header/UserIcon.jsx
+++ components/UI/Header/UserIcon.jsx
@@ -1,15 +1,32 @@
-import { UserIcon as Icon } from "@heroicons/react/solid";
+import { ChevronRightIcon, UserIcon as Icon } from "@heroicons/react/solid";
+import { setCookies } from "cookies-next";
+import { USER_TOKEN } from "../../../constants/common";
+import { HOME } from "../../../constants/routes";
 import useApp from "../../../hooks/useApp";
 
 const UserIcon = () => {
-    const [appState] = useApp();
+    const [appState, setAppState] = useApp();
 
-    // if (!appState.user) return null;
+    if (!appState.user) return null;
+
+    const handleLogOut = () => {
+        setAppState({
+            ...appState,
+            user: null,
+            isGuestUser: true
+        });
+        setCookies(USER_TOKEN, "");
+        window.location.href = HOME;
+    }
 
     return (
-        <div className="flex items-center cursor-pointer">
+        <div className="flex items-center cursor-pointer space-x-3 justify-center lg:justify-start" onClick={handleLogOut}>
             <div className="flex items-center justify-center bg-gray-400 rounded-full h-12 w-12">
-                <Icon className="text-white text-2xl uppercase w-6 h-6" />
+                <Icon className="text-white uppercase w-6 h-6" />
+            </div>
+            <div className="text-base font-semibold flex items-center">
+                {appState.user.username}
+                <ChevronRightIcon className="w-7 h-7 text-black" />
             </div>
         </div>
     );
